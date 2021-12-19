82e5826b61de3b4a81d5e48edb8694f8966d2178
Civan Erbay <civan.erbay@web.de>
2021-12-19 19:39:01 +0100

    Add Add explosion animation

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,36 +1,62 @@
 using UnityEngine;
-using System;
+using System.Collections;
+using System.Collections.Generic;
 
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
-    bool launch = false;
+    private Animator anim;
+
+    float currentSpeed = 0f;
+    float maxSpeed = 100f;
+    bool launched = false;
+    public float movementSpeed = 15.0f;
+    public float accelerationTime = 60;     
+    private float minSpeed ;
+    private float time ;
 
 
     private void Awake() {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        minSpeed = currentSpeed; 
+        time = 0 ;
+    }
+
+    private void FixedUpdate() {
+        body.AddForce(Physics.gravity*body.mass);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(launched == true) {
+        anim.Play("ExplosionAnim");
+        Destroy(gameObject, 0.5f);
+        }
     }
     
     private void Update() {
-        float horizontalInput = Input.GetAxis("Vertical");
-      
-        body.velocity = new Vector2(body.velocity.x, horizontalInput * speed);
+        float verticalInput = Input.GetAxisRaw("Vertical");
+    
+       currentSpeed = Mathf.SmoothStep(minSpeed, maxSpeed, time / accelerationTime );
+  
+        time += Time.deltaTime ;
+       // Debug.Log("currentSpeed = " + currentSpeed);
+      //  Debug.Log("Player Posi y Axis = " + body.transform.position.y);
+
+        if (verticalInput == 1) {
+            body.velocity = new Vector2(body.velocity.x, verticalInput * currentSpeed ); 
+        } 
+        
+        if (verticalInput == 1 && body.transform.position.y > 100) {
+            launched = true;
+        }
 
 
-         if(Input.GetKey(KeyCode.Space)) {
+       /*  if(Input.GetKey(KeyCode.Space)) {
             body.velocity = new Vector2(body.velocity.x, speed);
             launch = true;
-            }
-       
-         Debug.Log("launch = " + launch);
-        //Turn Rocket
-        if(horizontalInput < 0.01f && launch ) {
-            Debug.Log(launch);
-            transform.localScale = new Vector2(-1, -1);
-            }
-
-       
+            } */      
     }
 }
