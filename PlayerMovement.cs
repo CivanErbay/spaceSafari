dec10eaf7195226f3eff500dcce5f6719085f90c
Civan Erbay <civan.erbay@web.de>
2021-12-22 12:30:53 +0100

    Add rework speed calc and refactor code

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -2,74 +2,105 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField]
+    private float speed;
     private Rigidbody2D body;
     private Animator anim;
     public GameObject rocket;
     public GameObject floor;
 
-    float currentSpeed = 0f;
+    float currentSpeed = 0.01f;
     float maxSpeed = 100f;
-    bool launched = false;
+    public bool launched = false;
     public float movementSpeed = 15.0f;
-    public float accelerationTime = 60;     
-    private float minSpeed ;
-    private float time ;
+    public float accelerationTime = 60;
+    private float minSpeed;
+    private float time;
     private float distanceHeight;
 
+    //Custom Functions
+    private void handleVertical(
+        float verticalInput,
+        float currentSpeed,
+        float maxSpeed,
+        Rigidbody2D body,
+        Animator anim
+    )
+    {
+        if (verticalInput == 1)
+        {
+            if (currentSpeed <= maxSpeed)
+            {
+                this.currentSpeed = currentSpeed + 0.1f;
+            }
 
-    private void Awake() {
+            Debug.Log(currentSpeed);
+
+            body.velocity = new Vector2(body.velocity.x, verticalInput * currentSpeed);
+            anim.Play("accelerationAnim");
+        }
+        else if (verticalInput == 0)
+        {
+            if (currentSpeed >= 0)
+            {
+                this.currentSpeed = currentSpeed - 0.1f;
+            }
+            anim.Play("IdleAnim");
+        }
+        if (verticalInput == 1 && body.transform.position.y > 100)
+        {
+            launched = true;
+        }
+    }
+
+    private void handleHeight(GameObject rocket, GameObject floor)
+    {
+        if (rocket && floor)
+        {
+            Vector3 distanceVector = rocket.transform.position - floor.transform.position;
+            this.distanceHeight = distanceVector.y;
+        }
+    }
+
+    //BuiltIn-Functions
+    private void Awake()
+    {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         rocket = GameObject.Find("Player");
         floor = GameObject.Find("Ground");
-        minSpeed = currentSpeed; 
-        time = 0 ;
+        minSpeed = currentSpeed;
     }
 
-    private void Start() {
-         PlayerPrefs.SetInt ("PlayerHeight", 0);
+    private void Start()
+    {
+        PlayerPrefs.SetInt("PlayerHeight", 0);
     }
 
-    private void FixedUpdate() {
-        body.AddForce(Physics.gravity*body.mass);
+    private void FixedUpdate()
+    {
+        body.AddForce(Physics.gravity * body.mass);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if(launched == true) {
-        anim.Play("ExplosionAnim");
-        Destroy(gameObject, 4.5f);
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (launched == true)
+        {
+            anim.Play("ExplosionAnim");
+            Destroy(gameObject, 4.5f);
         }
     }
-    
-    private void Update() {
+
+    private void Update()
+    {
         float verticalInput = Input.GetAxisRaw("Vertical");
-        time += Time.deltaTime ;
-
-
-        if (verticalInput == 1) {
-            currentSpeed = Mathf.SmoothStep(minSpeed, maxSpeed, accelerationTime / time );
-            body.velocity = new Vector2(body.velocity.x, verticalInput * currentSpeed  ); 
-            anim.Play("accelerationAnim");
-        } else if (verticalInput == 0) {
-            time = 0;
-            currentSpeed = 0f;
-            anim.Play("IdleAnim");
-        }
-   
-        if (verticalInput == 1 && body.transform.position.y > 100) {
-            launched = true;
-        }     
-
-        if  (rocket && floor) {
-            Vector3 delta = rocket.transform.position - floor.transform.position;
-            distanceHeight = delta.y;
-        }
-
-        Debug.Log(body.velocity.y);
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        //time += Time.deltaTime ;
+        handleVertical(verticalInput, currentSpeed, maxSpeed, body, anim);
+        handleHeight(rocket, floor);
 
         // Set current height and velocity to display them on screen
-        PlayerPrefs.SetInt("PlayerHeight", (int) distanceHeight);
-        PlayerPrefs.SetInt("PlayerVelocity", (int) currentSpeed);
+        PlayerPrefs.SetInt("PlayerHeight", (int)distanceHeight);
+        PlayerPrefs.SetInt("PlayerVelocity", (int)currentSpeed);
     }
 }
